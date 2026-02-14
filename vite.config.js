ca064e0ad14ce4cc4ecfdefaa14deb35804ccd40
Abhay Jaiswal <YOUR_GITHUB_EMAIL>
2026-02-14 14:38:32 +0530

    fix github pages white screen

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Refer to this main Documentation - https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: "/BeMyValentine/",
 })
-
